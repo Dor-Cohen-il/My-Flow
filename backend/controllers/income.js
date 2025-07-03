a1ee39c744324941a8d4e172d7bbe22ee665ff5b
Dor-Cohen-il <cohen.dor@icloud.com>
2025-07-04 00:40:21 +0300

    50.37

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -1,5 +1,53 @@
+const IncomeSchema = require("../models/incomeModel")
 
 
 exports.addIncome = async (req, res) => {
-    console.log(req.body);
+    const {title, amount, type, category, description, date} = req.body
+
+    const income = IncomeSchema({
+        title,
+        amount,
+        type,
+        category,
+        description,
+        date
+    })
+
+    try {
+        //Validations
+        if(!title || !category || !description || !date){
+            return res.status(400).json({message: 'All fields are required'})
+        }
+        if(amount <= 0 || !amount=== 'number'){
+            return res.status(400).json({message: 'Amount must be a positive number'})
+        }
+        await income.save()
+        res.status(200).json({message: 'Income added'})
+    } catch (error) {
+        res.status(500).json({message: 'Server error'})
+
+    }
+    console.log(income);
+}
+
+exports.getIncomes = async (req, res) =>{
+    try {
+        const incomes = await IncomeSchema.find().sort({createdAt: -1})
+        res.status(200).json(incomes)
+    } catch (error) {
+        res.status(500).json({message: 'Server error'});
+    }
+}
+
+exports.deleteIncome = async (req, res) =>{
+    const {id} = req.params;
+    console.log(id);
+    IncomeSchema.findByIdAndDelete(id)
+    .then((income) =>{
+        res.status(200).json({message: 'Income deleted'})
+    })
+    .catch((err) =>{
+        res.status(500).json({message: 'Server error'});
+    })
+
 }
--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,12 +1,13 @@
-const { addIncome } = require('../controllers/income')
+const { addIncome, getIncomes, deleteIncome } = require('../controllers/income')
 
 
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the API v1!' });
-});
+//router.get('/', (req, res) => {
+    //res.status(200).json({ message: 'Welcome to the API v1!' });
+//});
 
 router.post('/add-income', addIncome)
-
+    .get('/get-income', getIncomes)
+    .delete('/delete-income/:id', deleteIncome)
 module.exports = router;
